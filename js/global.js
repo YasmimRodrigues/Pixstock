a955b38b30663448d953979e99f951409f456e18
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-07 10:17:42 -0300

    feat(ui): implement sticky header and ripple interaction effect

--- js/global.js
+++ js/global.js
@@ -4,3 +4,27 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Header on-scroll state
+ */
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList} */ $rippleElems = document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem));
