d4cbb5370dc742abaf619009bf4fc16f649a9334
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-09 10:23:59 -0300

    feat(utils): add helper to attach events to multiple elements

--- js/utils/event.js
+++ js/utils/event.js
@@ -4,3 +4,14 @@
  */
 
 "use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType Event type eg. "click"
+ * @param {Function} callback Callback function
+ */
+
+export const addEventOnElements = function ($elements, eventType, callback) {
+    $elements.forEach($element => $element.addEventListener(eventType, callback));
+}
