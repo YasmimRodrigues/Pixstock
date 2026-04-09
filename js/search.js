b36a19b2b723c2b3ba43a152cb9db17726fc0dc4
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-09 10:34:20 -0300

    feat: add search view toggle and search field management

--- js/search.js
+++ js/search.js
@@ -4,3 +4,36 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { ripple } from "./utils/ripple.js";
+import { addEventOnElements } from "./utils/event.js";
+
+/**
+ * Search view toggle in small devices
+ */
+
+const /** {NodeList} */ $searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const /** {NodeElement} */ $searchView = document.querySelector("[data-search-view]");
+
+addEventOnElements($searchTogglers, "click", () => $searchView.classList.toggle("show"));
+
+/**
+ * Search clear
+ */
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchClearBtn = document.querySelector("[data-search-clear-btn]");
+
+$searchClearBtn.addEventListener("click", () => $searchField.value = "");
+
+/**
+ * Search type
+ */
+
+const /** {NodeElement} */ $searchSegment = document.querySelector("[data-segment='search']");
+const /** {NodeElement} */ $activeSegmentBtn = $searchSegment.querySelector("[data-segment-btn].selected");
+window.searchType = $activeSegmentBtn.dataset.segmentValue;
