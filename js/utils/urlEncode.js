6e9ddf8a5a7688f3654578b68968ea1cd990cf6b
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-17 10:59:09 -0300

    feat: implement urlEncode utility with regex-based parameter formatting

--- js/utils/urlEncode.js
+++ js/utils/urlEncode.js
@@ -4,3 +4,13 @@
  */
 
 "use strict";
+
+/**
+ * Convert Object to Url
+ * @param {Object} urlObj url object
+ * @returns url string
+ */
+
+export const urlEncode = urlObj => {
+    return Object.entries(urlObj).join("&").replace(/,/g, "=").replace(/#/g, "%23");
+}
