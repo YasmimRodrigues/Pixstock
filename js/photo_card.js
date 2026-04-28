145177905f1ff1a3389d91dabf74860713ae9a48
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-28 11:05:23 -0300

    feat: create photoCard component and implement initial data destructuring

--- js/photo_card.js
+++ js/photo_card.js
@@ -4,3 +4,31 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Create photo card
+ */
+
+export const photoCard = photo => {
+    const /** {String} */ root = window.location.origin;
+    console.log(photo);
+
+    const {
+        alt, 
+        avg_color: backdropColor,
+        width,
+        height,
+        id,
+        src: { large }
+    } = photo;
+
+    const /** {NodeElement} */ $card = document.createElement("div");
+    $card.classList.add("card", "grid-list");
+    $card.style.backgroundColor = backdropColor;
+}
