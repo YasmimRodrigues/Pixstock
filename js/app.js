0ac0100cd1889b6832b8fff3d64cd031c0d3ecd7
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-28 09:33:43 -0300

    feat: implement dynamic photo gallery rendering from Pexels API

--- js/app.js
+++ js/app.js
@@ -4,3 +4,27 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { client } from "./api_configure.js";
+
+/**
+ * Render curated photos in home page
+ */
+
+const /** {NodeElement} */ $photoGrid = document.querySelector("[data-photo-grid]");
+
+$photoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.photos.curated({ page: 1, per_page: 20}, data => {
+    console.log(data);
+    $photoGrid.innerHTML = "";
+
+    data.photos.forEach(photo => {
+        const /** {NodeElement} */ $photoCard = photoCard(photo);
+    });
+
+});
