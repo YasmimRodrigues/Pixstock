9d0bb5c4323f603325e4caaaf7e2f5eb580f93e3
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-04-30 09:13:14 -0300

    feat: implement dynamic masonry grid layout using gridInit and updateGrid utilities

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@
 
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
+import {gridInit, updateGrid} from "./utils/masonry_grid.js";
 
 /**
  * Render curated photos in home page
@@ -21,13 +22,15 @@ const /** {NodeElement} */ $photoGrid = document.querySelector("[data-photo-grid
 $photoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
 
 client.photos.curated({ page: 1, per_page: 20}, data => {
-    console.log(data);
+
     $photoGrid.innerHTML = "";
+    const /** {Object} */ photoGrid = gridInit($photoGrid);
 
     data.photos.forEach(photo => {
+
         const /** {NodeElement} */ $photoCard = photoCard(photo);
 
-        $photoGrid.appendChild($photoCard);
+        updateGrid($photoCard, photoGrid.$columnsHeight, photoGrid.$columns);
     });
 
 });
