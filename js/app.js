d05f4c6cba5ba5c54064c6cbee29bd767abec5cc
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-05-04 10:35:24 -0300

    feat: implement popular videos rendering and integrate videoCard with masonry grid

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
 import {gridInit, updateGrid} from "./utils/masonry_grid.js";
+import { videoCard } from "./video_card.js";
 
 /**
  * Render curated photos in home page
@@ -31,6 +32,29 @@ client.photos.curated({ page: 1, per_page: 20}, data => {
         const /** {NodeElement} */ $photoCard = photoCard(photo);
 
         updateGrid($photoCard, photoGrid.$columnsHeight, photoGrid.$columns);
+    });
+
+});
+
+/**
+ * Render popular videos in home page
+ */
+
+const /** {NodeElement} */ $videoGrid = document.querySelector("[data-video-grid]");
+
+$videoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.videos.popular({ per_page: 20 }, data => {
+
+    $videoGrid.innerHTML = "";
+    const /** {Array} */ videoGrid = gridInit($videoGrid);
+
+    data.videos.forEach(video => {
+
+        const /** {NodeElement} */ $videoCard = videoCard(video);
+
+        updateGrid($videoCard, videoGrid.$columnsHeight, videoGrid.$columns);
+
     });
 
 });
