9ec06cf7c7dcda8039c61f8f4f35260c2d6f7078
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-05-23 16:31:19 -0300

    feat: implement photos.js with dynamic title handling and filter bar toggle

--- pages/photos/photos.js
+++ pages/photos/photos.js
@@ -4,3 +4,40 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { client } from "../../js/api_configure.js";
+import { gridInit, updateGrid } from "../../js/utils/masonry_grid.js";
+import { photoCard } from "../../js/photo_card.js";
+import { updateUrl } from "../../js/utils/updateUrl.js";
+import { urlDecode } from "../../js/utils/urlDecode.js";
+
+/**
+ * Show filter bar if searched anything
+ */
+
+const /** {NodeElement} */ $filterBar = document.querySelector("[data-filter-bar]");
+
+$filterBar.style.display = window.location.search ? "flex" : "none";
+
+/**
+ * Render curated or searched photos
+ * If searched something then render searched photos
+ * Otherwise render curated photos
+ */
+
+const /** {NodeElement} */ $photoGrid = document.querySelector("[data-photo-grid]");
+const /** {NodeElement} */ $title = document.querySelector("[data-title]");
+const /** {Object} */ photoGrid = gridInit($photoGrid);
+const /** {Number} */ perPage = 30;
+let /** {Number} */ currentPage = 1;
+let /** {Number} */ totalPage = 0;
+const /** {String} */ searchUrl = window.location.search.slice(1);
+let /** {Object} */searchObj = searchUrl && urlDecode(searchUrl);
+const /** {String} */ title = searchObj ? `${searchObj.query} photos` : "Curated photos";
+
+$title.textContent = title;
+document.title = title;
