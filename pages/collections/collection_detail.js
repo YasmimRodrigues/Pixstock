d071d141b6b53754aa5f3e6f10cde44031a8dbc1
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-06-12 22:08:45 -0300

    feat(collections): initialize collection_detail.js with masonry grid and url helpers

--- pages/collections/collection_detail.js
+++ pages/collections/collection_detail.js
@@ -4,3 +4,25 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { client } from "../../js/api_configure.js";
+import { gridInit, updateGrid } from "../../js/utils/masonry_grid.js";
+import { photoCard } from "../../js/photo_card.js";
+import { videoCard } from "../../js/video_card.js";
+import { urlDecode } from "../../js/utils/urlDecode.js";
+
+/**
+ * Render collection medias
+ */
+
+const /** {NodeElement} */ $collectionGrid = document.querySelector("[data-collection-grid]");
+const /** {NodeElement} */ $title = document.querySelector("[data-title]");
+const /** {Object} */ collectionGrid = gridInit($collectionGrid);
+const /** {Number} */ perPage = 30;
+let /** {Number} */ currentPage = 1;
+let /** {Number} */ totalPage = 0;
+const /** {Object} */ collectionObj = urlDecode(window.location.search.slice(1));
