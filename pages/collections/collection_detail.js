e5ec74749755d2381160c83108288da2e8cfc27c
YasmimRodrigues <yasmimrbm25@gmail.com>
2026-06-14 17:56:07 -0300

    feat:implement hybrid media rendering with switch case type matching and infinite scroll

--- pages/collections/collection_detail.js
+++ pages/collections/collection_detail.js
@@ -26,3 +26,62 @@ const /** {Number} */ perPage = 30;
 let /** {Number} */ currentPage = 1;
 let /** {Number} */ totalPage = 0;
 const /** {Object} */ collectionObj = urlDecode(window.location.search.slice(1));
+
+$title.textContent = `${collectionObj.title} collections`;
+document.title = `${collectionObj.title} collections`;
+
+/**
+ * 
+ * @param {Number} page Current page
+ */
+
+const loadCollection = function (page) {
+
+    client.collections.detail(collectionObj.collectionId, { per_page: perPage, page: page }, data => {
+
+        totalPage = Math.ceil(data.total_results / perPage);
+
+        data.media.forEach(item => {
+            
+            let /** {NodeElement} */ $card;
+
+            switch (item.type.toLowerCase()) {
+                case "photo":
+                    $card = photoCard(item);
+                    break;
+                case "video":
+                    $card = videoCard(item);
+                    break;
+            }
+
+            updateGrid($card, collectionGrid.$columnsHeight, collectionGrid.$columns);
+
+            isLoaded = true;
+            if(currentPage >= totalPage) $loader.style.display = "none";
+            
+        });
+
+    });
+
+}
+
+loadCollection(currentPage);
+
+/**
+ * Load more collections
+ */
+
+const /** {NodeElement} */ $loader = document.querySelector("[data-loader]");
+let /** {Boolean} */ isLoaded = true;
+
+window.addEventListener("scroll", function () {
+
+    if($loader.getBoundingClientRect().top < (window.innerHeight * 2) && currentPage <= totalPage && isLoaded) {
+   
+        currentPage++;
+        loadCollection(currentPage);
+        isLoaded = false;
+
+    }
+
+});
